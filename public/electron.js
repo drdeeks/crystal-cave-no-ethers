4484dad140903c701562a32b02b522341cc18ff1
drdeeks <drdeeksuniqueantiques@gmail.com>
2025-07-06 23:04:29 -0700

    Update electron.js to use app.isPackaged for development mode detection

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
-const isDev = require('electron-is-dev');
+const isDev = !app.isPackaged;
 
 function createWindow() {
   // Create the browser window
